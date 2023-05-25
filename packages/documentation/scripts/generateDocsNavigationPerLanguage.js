11298662fbcda23b3371c91d516c692a19ee1550
Jack <jack20220723@gmail.com>
2023-05-25 21:33:42 +0800

    Document DI tools (#6)
    * Document DI tools
    
    * Do not run Azure deploy preview in fork
    
    * Do not run Azure deploy preview in fork

--- packages/documentation/scripts/generateDocsNavigationPerLanguage.js
+++ packages/documentation/scripts/generateDocsNavigationPerLanguage.js
@@ -91,6 +91,7 @@ const handbookPages = [
       { file: "reference/Type Compatibility.md" },
       { file: "reference/Type Inference.md" },
       { file: "reference/Variable Declarations.md" },
+      { file: "reference/Dependency Injection Containers.md" },
     ],
   },
   {
